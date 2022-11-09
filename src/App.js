48fd88b1a28c9e9062aac40a5203f4505bf8f20e
mpeter35 <77943660+mpeter35@users.noreply.github.com>
2022-11-09 15:21:59 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,28 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([])
+  const [count, setCount] = useState(0)
+  
+  
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Maddie's Yummy Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem info={item} num={index} cart={cart} setCart={setCart} count={count} setCount={setCount}/> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {cart.map(item => <p>{item}</p>)}
+        <h3> Total Cost ${count}</h3>
+        <div>
+
+		</div>
+
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+function BakeryItem(props){
+    const handleClick = () =>{
+        // props.setCart(props.cart + 1)
+        props.setCart( arr => [...arr, props.info.name])
+        props.setCount(Math.round((props.count + props.info.price+ Number.EPSILON)*100)/100)
+        
+    }
+    return <div>
+        <h1>{props.info.name}</h1>
+        <h3>{props.info.description}</h3>
+        <img style={{ width: "20%", height: "30%" }} src = {props.info.image}/> 
+        <button onClick={handleClick}> Add to Cart </button>
+        <h3 style={{color: "red"}} >{props.info.price}</h3>
+    </div>;
+}
+
+
+
+
+
+export default BakeryItem
